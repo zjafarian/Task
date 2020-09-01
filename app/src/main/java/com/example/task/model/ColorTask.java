95b13589132c671ff1e07929ef9ec7ba7981b328
zjafarian <zjafarian88@gmail.com>
2020-09-01 17:42:32 +0430

    Complete TaskRepository

--- app/src/main/java/com/example/task/model/ColorTask.java
+++ app/src/main/java/com/example/task/model/ColorTask.java
@@ -1,5 +1,5 @@
 package com.example.task.model;
 
 public enum ColorTask {
-    BlueGreyLight, BlueGreyDark;
+    TealLight, TealDark;
 }
--- app/src/main/java/com/example/task/repository/TaskRepository.java
+++ app/src/main/java/com/example/task/repository/TaskRepository.java
@@ -1,8 +1,12 @@
 package com.example.task.repository;
 
+import com.example.task.model.ColorTask;
+import com.example.task.model.State;
 import com.example.task.model.Task;
 
 import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 public class TaskRepository {
     private static TaskRepository sInstance;
@@ -11,9 +15,30 @@ public class TaskRepository {
     private static String sNameTask;
 
 
+    private TaskRepository() {
+        for (int i = 0; i < sIntSize; i++) {
+            State state = generateRandomState();
+            ColorTask colorTask;
+            if (i % 2 == 0) {
+                colorTask = ColorTask.TealLight;
+            } else colorTask = ColorTask.TealDark;
+            Task task = new Task(sNameTask, state, colorTask);
+            mTaskList.add(task);
+        }
+
+    }
+
+    private State generateRandomState() {
+        State[] states = State.values();
+        int length = states.length;
+        int rndIndex = new Random().nextInt(length);
+        return states[rndIndex];
+    }
+
     public static TaskRepository getInstance() {
         return sInstance;
     }
+
 
     public static void setInstance(TaskRepository instance) {
         sInstance = instance;
@@ -42,4 +67,23 @@ public class TaskRepository {
     public static void setNameTask(String nameTask) {
         sNameTask = nameTask;
     }
+
+    public static void setSizeTaskAndNameTask(int sSize, String sName) {
+        sIntSize = sSize;
+        sNameTask = sName;
+    }
+
+    public void insertTask (Task task){
+        mTaskList.add(task);
+    }
+
+    public Task getTask (UUID uuid){
+        for (Task task: mTaskList) {
+            if (task.getUUIDTask().equals(uuid))
+                return task;
+        }
+        return null;
+    }
+
+
 }
