822c640876d1857bf677218f43f60d76b9f4b731
zjafarian <zjafarian88@gmail.com>
2020-09-01 19:53:02 +0430

    Write logic CreateTaskFragment and Intent

--- app/src/main/java/com/example/task/controller/fragment/CreateTaskFragment.java
+++ app/src/main/java/com/example/task/controller/fragment/CreateTaskFragment.java
@@ -1,16 +1,27 @@
 package com.example.task.controller.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.task.R;
+import com.example.task.controller.activity.TaskListActivity;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class CreateTaskFragment extends Fragment {
+    public static final String EXTRA_SEND_NAME_TASK = "com.example.task.sendNameTask";
+    public static final String EXTRA_SEND_NUMBER_TASK = "com.example.task.sendNumberTask";
+    private TextInputEditText mTextNameTask;
+    private TextInputEditText mTextNumber;
+    private Button mButtonCreate;
 
 
     public CreateTaskFragment() {
@@ -28,6 +39,34 @@ public class CreateTaskFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_create_task, container, false);
+        setView(view);
+        setListener();
         return view;
     }
+    private void setView(View view) {
+        mTextNameTask= view.findViewById(R.id.text_input_name);
+        mTextNumber = view.findViewById(R.id.text_input_number);
+        mButtonCreate=view.findViewById(R.id.btn_create);
+    }
+
+    private void setListener() {
+        mButtonCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mTextNameTask.getText().equals("") || mTextNumber.getText().equals("")){
+                    Toast toast = Toast.makeText(getActivity(),"",Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.BOTTOM,0,0);
+                    toast.show();
+                } else {
+                    Intent intent = new Intent(getActivity(), TaskListActivity.class);
+                    intent.putExtra(EXTRA_SEND_NAME_TASK,mTextNameTask.getText());
+                    intent.putExtra(EXTRA_SEND_NUMBER_TASK,mTextNumber.getText());
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+
+
 }
